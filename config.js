e885df32604ed332c1778882481dafb65476201c
iarigby <marvinzem@gmail.com>
2020-01-03 04:17:05 +0400

    fix the absolutely brain damaged getId()

--- config.js
+++ config.js
@@ -2,7 +2,9 @@
 const backend = 'http://116.203.187.241:3000'
 const getId = function() {
     const path = window.location.href.toString()
-    return path.substring(path.indexOf('?id=') + 4, path.length)
+    const header = path.indexOf('#')
+    const pos = header > 0 ? header : path.length
+    return path.substring(path.indexOf('?id=') + 4, pos)
 }
 
 const studentId = getId()
