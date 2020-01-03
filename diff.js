88724b69537b70ab5fb87b85408ae47f07c376fc
iarigby <marvinzem@gmail.com>
2020-01-03 12:52:41 +0400

    improve view ^^

--- diff.js
+++ diff.js
@@ -1,6 +1,7 @@
 function addDiff(diffString) {
     var diffHtml = Diff2Html.getPrettyHtml(
-        diffString, { inputFormat: 'diff', showFiles: true, matching: 'lines', outputFormat: 'side-by-side' }
+        diffString, { inputFormat: 'diff', showFiles: true, matching: 'lines' }
+        //, outputFormat: 'side-by-side' 
     );
     document.getElementById('diffElement').innerHTML = diffHtml;
 }
