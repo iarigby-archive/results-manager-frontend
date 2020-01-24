2de5061067e7cfbf1dbf0b9a85eeece3e74f5032
iarigby <marvinzem@gmail.com>
2020-01-24 15:29:53 +0400

    what is env :S what is prod :S

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-const backend = 'http://localhost:3000'
+let backend = 'http://localhost:3000'
+
+function getPath() {
+    window.location.href.toString()
+}
     // const backend = 'http://116.203.187.241:3000'
 const getId = function() {
-    const path = window.location.href.toString()
+    const path = getPath()
     const header = path.indexOf('#')
     const pos = header > 0 ? header : path.length
     const id = path.indexOf('?id=')
@@ -9,3 +13,5 @@ const getId = function() {
 }
 
 const studentId = getId()
+if (getPath().includes('116.203.187.241'))
+    backend = 'http://116.203.187.241:3000'  
